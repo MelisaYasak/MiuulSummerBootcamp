09d5313fc5ce5cb0d6c375c93a5196f874a51525
Melisa Yasak <melisa.yasak@bil.omu.edu.tr>
2023-10-24 20:02:09 +0300

    finish case study3

--- pythonProgramming/cs_3.py
+++ pythonProgramming/cs_3.py
@@ -2,7 +2,7 @@
 import seaborn as sns
 import matplotlib.pyplot as plt 
 
-titanic = sns. load_dataset('titanic')
+titanic = sns.load_dataset('titanic')
 print(titanic.head())
 
 # Task 2: Find the number of male and female passengers in the Titanic data set.
@@ -61,3 +61,68 @@ print(titanic["age"])
 # Task 15: Find the sum, count, mean values of the survived variable in the breakdown of pclass and gender variables.
 result = titanic.groupby(['pclass', 'sex'])['survived'].agg(['sum', 'count', 'mean'])
 print(result)
+
+# Task 16: Write a function that will give 1 to those under 30 and 0 to those equal to or above 30. Create a variable named age_flag in titanic dataset using the function you wrote (use apply and lambda structures).
+def age_flag(age):
+    return 1 if age > 30 else 0
+
+titanic['age_flag'] = titanic['age'].apply(lambda x: age_flag(x))
+print(titanic[['age', 'age_flag']].head())
+
+# Task 17: Define the Tips data set from the Seaborn library.
+tips = sns.load_dataset("tips")
+print(tips.head())
+
+# Task 18: Find the sum, min, max and average of the total_bill values according to the categories (Dinner, Lunch) of the variable Time.
+grouped = tips.groupby("time")["total_bill"]
+
+total_sum = grouped.sum()
+min_value = grouped.min()
+max_value = grouped.max()
+mean_value = grouped.mean()
+
+
+print("Toplam Bill:")
+print(total_sum)
+print("\nMinimum Bill:")
+print(min_value)
+print("\nMaksimum Bill:")
+print(max_value)
+print("\nOrtalama Bill:")
+print(mean_value)
+
+
+# Task 19: Find the sum, min, max and average of total_bill values by days and time.
+grouped = tips.groupby(["time","day"])["total_bill"]
+
+total_sum = grouped.sum()
+min_value = grouped.min()
+max_value = grouped.max()
+mean_value = grouped.mean()
+
+
+print("Toplam Bill:")
+print(total_sum)
+print("\nMinimum Bill:")
+print(min_value)
+print("\nMaksimum Bill:")
+print(max_value)
+print("\nOrtalama Bill:")
+print(mean_value)
+
+
+# Task 20: Find the sum, min, max and average of the total_bill and type values for Lunch time and female customers by day.
+grouped = tips[(tips["time"] == "Lunch") & (tips["sex"] == "Female" )].groupby("day")[["total_bill", "tip"]].agg(["sum", "min", "max", "mean"])
+print(grouped)
+
+# Task 21: What is the average of orders with size less than 3 and total_bill greater than 10? (use loc)
+result = tips.loc[(tips["size"]<3)&(tips['total_bill']>10)].mean()
+print(result)
+
+# Task 22: Create a new variable called total_bill_tip_sum. Give the sum of totalbill and type paid by each customer.
+def calculate_total_bill_tip_sum(row):
+  return row['total_bill'] + row['tip']
+
+# Task 23: Sort the total_bill_tip_sum variable from largest to smallest and assign the first 30 people to a new dataframe.
+sorted_total_bill_tip_sum = tips.sort_values(by = "total_bill_tip_sum", ascending=False)
+top_thrity = sorted_total_bill_tip_sum.iloc[:,:30]
