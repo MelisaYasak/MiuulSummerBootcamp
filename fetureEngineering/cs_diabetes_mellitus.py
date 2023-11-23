905eb8db62ba026a908d9dc1857466f222a470d4
Melisa Yasak <melisa.yasak@bil.omu.edu.tr>
2023-11-23 23:47:46 +0300

    Create feature engineering folder

---
+++ fetureEngineering/cs_diabetes_mellitus.py
@@ -0,0 +1,10 @@
+##########################################
+#
+#  Author:  Melisa YASAK
+#
+#  Date:    2019-07-31
+#
+# MAKING THE DATA READY FOR THE MODEL.
+# To perform data analysis and feature engineering steps for a machine learning model that can predict whether people have diabetes. 
+##########################################
+
